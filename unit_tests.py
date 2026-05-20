b1cc1bf12ac6c97091a2830e26adfd8d4e497184
danitoneva <d.toneva2003@gmail.com>
2026-05-20 17:00:33 +0300

    unit tests

---
+++ unit_tests.py
@@ -0,0 +1,24 @@
+import unittest
+import my_math
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(my_math.add(1, 2), 3)
+        self.assertEqual(my_math.add(-1, -1), -2)
+        self.assertEqual(my_math.add(-1, 2), 1)
+
+    def test_substract(self):
+        self.assertEqual(my_math.subtract(2, 1), 1)
+        self.assertEqual(my_math.subtract(1, 2), -1)
+        self.assertEqual(my_math.subtract(0, 2), -2)
+
+    def test_multiply(self):
+        self.assertEqual(my_math.multiply(2, 3), 6)
+        self.assertEqual(my_math.multiply(-2, -2), 4)
+        self.assertEqual(my_math.multiply(0, 4), 0)
+
+    def test_divide(self):
+        self.assertEqual(my_math.divide(6, 2), 3)
+        self.assertEqual(my_math.divide(5, 2), 2.5)
+
+if __name__ == '__main__':
+    unittest.main()
